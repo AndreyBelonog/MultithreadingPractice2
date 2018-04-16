5764411ec2108c1a50280873612ddfaacf136d30
AndreyBelonog <36847003+AndreyBelonog@users.noreply.github.com>
2018-04-16 13:45:15 +0300

    Created Worker class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,65 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        new Worker().main();
+    }
+}
 
+class Worker{
+    Random random = new Random();
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void addToList1() {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        list1.add(random.nextInt());
+    }
+
+    public void addToList2(){
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        list2.add(random.nextInt());
+    }
+
+    public void work(){
+        for(int i = 0; i< 1000; i++) {
+            addToList1();
+            addToList2();
+        }
+    }
+
+    public void main(){
+        long before = System.currentTimeMillis();
+
+        Thread thread1 = new Thread(this::work);
+        Thread thread2 = new Thread(this::work);
+
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        }catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println("Program took " + (after - before) + " ms to run");
+        System.out.println("List1: " + list1.size());
+        System.out.println("List2: " + list2.size());
     }
 }
